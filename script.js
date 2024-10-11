482633ba02b9ac766fba6a6356e11933494add5e
Nayer <nayere@nycstudents.net>
2024-10-11 17:45:05 -0400

    user

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Nayer Ebraheim"; // update the part BETWEEN the "quotes"
+let userName = "nayere6342"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
